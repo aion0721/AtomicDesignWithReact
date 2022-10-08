a15ba80045a1b6ce323bf2b03b254a4df37e0b77
a <rudolf.aion@gmail.com>
2022-10-08 14:30:31 +0000

    create top button

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,8 +1,18 @@
 import styled from "styled-components";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
 export const Top = () => {
+  const onClickAdmin = () => {
+    alert("admin");
+  };
+  const onClickGeneral = () => {
+    alert("general");
+  };
   return (
     <SContainer>
       <h2>TOP</h2>
+      <SecondaryButton onClick={onClickAdmin}>Admin</SecondaryButton>
+      <br />
+      <SecondaryButton onClick={onClickGeneral}>Normal</SecondaryButton>
     </SContainer>
   );
 };
